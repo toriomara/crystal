3273383a3e62b98f9c6b24990cc99d927077912e
toriomara <budkevich.vladimir@gmail.com>
2022-02-17 23:05:55 +0300

    articles_21

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== 'production'
-export const server = dev ? 'http://localhost:3000' : 'https://crystal-three.vercel.app'
+export const server = dev ? 'http://localhost:3000' : 'https://crystal-three.vercel.app/'
