833c7e6816e6858021fc60def72836624228a0a5
toriomara <budkevich.vladimir@gmail.com>
2022-04-05 14:36:59 +0300

    correct link

--- components/Articles/ArticlesList.js
+++ components/Articles/ArticlesList.js
@@ -28,7 +28,7 @@ const ArticleList = ({ articles }) => {
         <MotionGridItem
           key={article.id}
           _hover={{
-            color: "brand.200",
+            color: useColorModeValue("brand.100", "brand.200"),
           }}
           whileHover={{
             position: "relative",
