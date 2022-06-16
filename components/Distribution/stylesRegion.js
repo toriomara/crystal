b0621abce94dfd9d9836eb59c65b69d3cde8cee7
toriomara <budkevich.vladimir@gmail.com>
2022-06-16 12:03:37 +0300

    burgerMenu

--- components/Distribution/stylesRegion.js
+++ components/Distribution/stylesRegion.js
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-//import { css, keyframes } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
 import { useColorModeValue } from '@chakra-ui/react';
 
 export const TabContainer = styled.div`
@@ -46,17 +46,17 @@ export const StyledTabPanel = styled.div`
   font-weight: 500;
 `;
 
-// export const inactiveTab = {
-//   opacity: 0.65,
-// };
+export const inactiveTab = {
+  opacity: 0.65,
+};
 
-// const inset = keyframes`
-//   0% {
-//     -webkit-box-shadow: inset 0 0 0 0 rgba(238, 82, 83, 0);
-//             box-shadow: inset 0 0 0 0 rgba(238, 82, 83, 0);
-//   }
-//   100% {
-//     -webkit-box-shadow: inset 0 0 14px 0px rgba(238, 82, 83, 0.95);
-//             box-shadow: inset 0 0 14px 0px rgba(238, 82, 83, 0.95);
-//   }
-// `;
+const inset = keyframes`
+  0% {
+    -webkit-box-shadow: inset 0 0 0 0 rgba(238, 82, 83, 0);
+            box-shadow: inset 0 0 0 0 rgba(238, 82, 83, 0);
+  }
+  100% {
+    -webkit-box-shadow: inset 0 0 14px 0px rgba(238, 82, 83, 0.95);
+            box-shadow: inset 0 0 14px 0px rgba(238, 82, 83, 0.95);
+  }
+`;
